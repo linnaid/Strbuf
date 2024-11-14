fe0b7a4867db1a8059e1d3eae5adf2c23728bd5c
linnaid <jiaguoyangyang@qq.com>
2024-11-14 16:22:12 +0800

    Your commit message

--- LinXY.c
+++ LinXY.c
@@ -2,35 +2,30 @@
 // 初始化 sb 结构体，容量为 alloc
 void strbuf_init(struct strbuf *sb, size_t alloc){
     sb->alloc = alloc;
-    if(sb == NULL)
-    return -1;
     sb->buf = (char*)malloc((alloc+1) * sizeof(char));
-    if(sb -> buf == NULL)
-    {
-        return -1;
-    }
-    return;
+    if(sb->buf == NULL)
+    sb->buf[0] = '\0';
+    //return;
 }
 // 将字符串填充到 sb 中，长度为 len，容量为 alloc
 void strbuf_attach(struct strbuf *sb, void *str, size_t len, size_t alloc){
     sb->alloc = alloc;
     sb->len = len;
-    for(int i = 0; i < len; i++)
-    {
-        sb->buf[i] = str[i];
-    }
+        sb->buf = (char*)str;
     sb->buf[len] = '\0';
-    return;
+    //return;
 }
 // 释放 sb 结构体的内存
 void strbuf_release(struct strbuf *sb){
     free(sb->buf);
     sb->buf = NULL;
-    return;
+    sb->alloc = 0;
+    sb->len = 0;
+    //return;
 }
 // 交换两个 strbuf
 void strbuf_swap(struct strbuf *a, struct strbuf *b){
-    strbuf swap;
+    struct strbuf swap;
     swap.len = a->len;
     swap.buf = a->buf;
     swap.alloc = a->alloc;
@@ -40,7 +35,7 @@ void strbuf_swap(struct strbuf *a, struct strbuf *b){
     b->alloc = swap.alloc;
     b->buf = swap.buf;
     b->len = swap.len;
-    return;
+    //return;
 }
 // 将 sb 中的原始内存取出，并将 sz 设置为其 alloc 大小
 char *strbuf_detach(struct strbuf *sb, size_t *sz){
@@ -62,70 +57,154 @@ void strbuf_reset(struct strbuf *sb){
     sb->buf[0] = '\0';
     sb->alloc = 0;
     sb->len = 0;
-    return;
+    //return;
 }
 
 // 确保在 len 之后 strbuf 中至少有 extra 个字节的空闲空间可用。
 void strbuf_grow(struct strbuf *sb, size_t extra){
-    if(sb->alloc < len + extra)
+    if(sb->alloc < sb->len + extra)
     {
         sb->alloc = sb->len + extra;
         char* sd = (char*)realloc(sb->buf,(sb->alloc + 1)*sizeof(char));
         if(sd == NULL)
-        return 0;
+        exit(0);
         sb->buf = sd;
     }
     return;
 }
 // 向 sb 追加长度为 len 的数据 data。
 void strbuf_add(struct strbuf *sb, const void *data, size_t len){
-    
-    return;
+    strbuf_grow(sb,len);
+    strncat(sb->buf, (char*)data, len);
+    sb->alloc = sb->alloc + len;
+    sb->len = sb->len + len;
 }
 // 向 sb 追加一个字符 c。
 void strbuf_addch(struct strbuf *sb, int c){
-    return;
+    strbuf_grow(sb,1);
+    sb->buf[sb->len] = 'c';
+    sb->buf[sb->len+1] = '\0';
+    sb->len = sb->len + 1;
+    sb->alloc = sb->alloc + 1;
 }
 // 向 sb 追加一个字符串 s。
 void strbuf_addstr(struct strbuf *sb, const char *s){
-    return;
+    size_t sz = strlen(s);
+    strbuf_grow(sb,sz);
+    while(*s != '\0')
+    {
+        sb->buf[sb->len] = *s++;
+        sb->len++;
+    }
+    sb->buf[sb->len] = '\0';
+    sb->alloc = sb->alloc + sz;
 }
 // 向一个 sb 追加另一个 strbuf 的数据。
 void strbuf_addbuf(struct strbuf *sb, const struct strbuf *sb2){
-    return;
+    strbuf_grow(sb,sb2->len);
+    size_t i = 0;
+    while(sb2->buf[i] != '\0')
+    {
+        sb->buf[sb->len++] = sb2->buf[i++];
+    }
+    sb->buf[sb->len] = '\0';
+    sb->alloc = sb->alloc + sb2->alloc;
 }
 // 设置 sb 的长度 len。
 void strbuf_setlen(struct strbuf *sb, size_t len){
-    return;
+    if(sb->len < sb->alloc)
+    sb->buf[sb->len] = '\0';
 }
 // 计算 sb 目前仍可以向后追加的字符串长度。
 size_t strbuf_avail(const struct strbuf *sb){
-    return;
+    size_t sz = sb->len;
+    return sb->alloc - sz - 1;
 }
 // 向 sb 内存坐标为 pos 位置插入长度为 len 的数据 data。
 void strbuf_insert(struct strbuf *sb, size_t pos, const void *data, size_t len){
-    return;
+    strbuf_grow(sb,len);
+    memmove(sb->buf + pos + len, sb->buf + pos,sb->len - pos);
+    size_t j = pos;
+    char* a = (char*)data;
+    for(size_t i = 0; i < len; i++)
+    {
+        //sb->buf[j++] = *((char*)data++);
+        sb->buf[j++] = *a++;
+    }
 }
 
 // 去除 sb 缓冲区左端的所有空格、制表符和'\t'字符。
 void strbuf_rtrim(struct strbuf *sb){
-    return;
+    size_t i = 0;
+    while(isspace((unsigned char)sb->buf[i]) && i < sb->len)
+    i++;
+    if(i > 0)
+    {
+    memmove(sb->buf, sb->buf + i, sb->len - i);
+    sb->len = sb->len - i;
+    sb->buf[sb->len] = '\0';
+    }
 }
 // 去除 sb 缓冲区右端的所有空格、制表符和'\t'字符。
 void strbuf_ltrim(struct strbuf *sb){
-    return;
-}
+    size_t i = sb->len - 1;
+    while(isspace((unsigned char)sb->buf[i-1]) && i > 0)
+    i--;
+    if(i < sb->len)
+    {
+        sb->buf[i] = '\0';
+        sb->len = i;
+        sb->alloc = i;
+    }
+    }
 // 删除 sb 缓冲区从 pos 坐标开始长度为 len 的内容。
 void strbuf_remove(struct strbuf *sb, size_t pos, size_t len){
-    return;
+    memmove(sb->buf + pos,sb->buf + pos + len,sb->len - pos - len);
+    sb->len = sb->len - len;
+    sb->alloc = sb->alloc - len;
 }
 
 // 将文件描述符为 fd 的所有文件内容追加到 sb 中。sb 增长 hint ? hint : 8192 大小。
 ssize_t strbuf_read(struct strbuf *sb, int fd, size_t hint){
-    return;
+    size_t add = hint?hint:8192;
+    char *aim = (char*)malloc(add);
+    if(aim == NULL)
+    return -1;
+    size_t total = 0;
+    while(1)
+    {
+        ssize_t read1 = read(fd,aim,add);
+        if(read1 < 0)
+        {
+            free(aim);
+            return -1;
+        }
+        else if(read1 == 0)
+        break;
+        else
+        {
+            if(sb->len + read1 > sb->alloc)
+            {
+                size_t alloc1 = sb->alloc + add;
+                sb->buf = (char*)realloc(sb->buf,alloc1); 
+                if(sb->buf == NULL)
+                {
+                    free(aim);
+                    return -1;
+                }
+                sb->alloc = alloc1;
+            }
+        }
+        memcpy(sb->buf + sb->len,aim,read1);
+        sb->len = sb->len + read1;
+        total = total + read1;
+    }
+    free(aim);
+    return total;
 }
 // 将文件句柄为 fp 的一行内容（抛弃换行符）读取到 sb。
 int strbuf_getline(struct strbuf *sb, FILE *fp){
+    
     return;
 }
 //实现字符串切割（C 系字符串函数的一个痛点）。
